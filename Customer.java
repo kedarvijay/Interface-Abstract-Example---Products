782b9a2f186c33f164cc29c01cc74c3edffce53b
Vijay Kedar <54878107+kedarvijay@users.noreply.github.com>
2022-01-26 23:14:59 +0530

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,11 @@
+
+public class Customer extends Sales {
+
+	public static void main(String[] args) {
+		Customer obj = new Customer();
+		System.out.println(obj.sale(100));
+		System.out.println(obj.purchase(10));
+
+	}
+
+}
